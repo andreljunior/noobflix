418f8d80857a4d08ccb74a84d64954e7a014a377
andreljunior <andrefsilvajr@gmail.com>
2020-07-28 13:30:05 -0300

    commit on master

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button'
-import ButtonLink from './components/ButtonLink';
+//import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return ( 
-    < nav className = "Menu" > 
-            <a href = "/" > 
+        <nav className = "Menu"> 
+            <Link to="/"> 
                 <img className = "Logo" src = {Logo} alt = "Noobflix logo" /> 
-            </a> 
-            <ButtonLink href="/" className="ButtonLink">
-                Novo vídeo
-            </ButtonLink>          
+            </Link> 
 
-    </nav> );
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo vídeo
+            </Button>          
+        </nav> 
+    );
 }
 export default Menu;
